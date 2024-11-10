a74b9a585354cae5e907e6a6559762ce9105d0aa
Joseph Jackson <josephjackson2002@yahoo.com>
2024-11-10 18:02:44 -0500

    Started my first react app. So far I have added my own List Group component with styles imported from bootstrap. Also made it interactive on click.

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+//Not using this at the moment!!!!!
+// PascalCasing (compared to normal camalCase)
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Joseph';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message; 
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+// {items: [], heading: string}
+interface Props {
+    items: string[];
+    heading: string;
+}
+
+function ListGroup({items, heading}: Props) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            onClick={() => {setSelectedIndex(index);}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
